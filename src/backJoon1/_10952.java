a9a97b80af341078081e4319cdcbfab43e7936f7
userNameIldo <ildo5896@naver.com>
2022-02-04 22:16:07 +0900

    02.04

---
+++ src/backJoon1/_10952.java
@@ -0,0 +1,27 @@
+package backJoon1;
+
+import java.util.Scanner;
+
+public class _10952 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int add;
+		
+		while(true) {
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			
+			if(num1 == 0 && num2 == 0) {
+				break;
+			}
+			
+			System.out.println(num1+num2);
+			
+		}
+
+	}
+
+}
