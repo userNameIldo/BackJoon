1dfaa54763b2b4632477e44b08ad97cba84ed792
userNameIldo <ildo5896@naver.com>
2022-02-19 23:02:54 +0900

    02.19

---
+++ src/backJoon1/_2869.java
@@ -0,0 +1,29 @@
+package backJoon1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2869 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+		
+		int i = (V-B) / (A - B);
+		
+		if((V-B)%(A-B) != 0) {
+			i++;
+		}
+		
+		System.out.println(i);	
+
+	}
+
+}
