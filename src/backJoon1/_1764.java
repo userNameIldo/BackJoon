a351fe3b63ab8b499fa7d6a27a2bf0f26ddad6a1
userNameIldo <ildo5896@naver.com>
2022-03-06 23:48:31 +0900

    03.06

---
+++ src/backJoon1/_1764.java
@@ -0,0 +1,45 @@
+package backJoon1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class _1764 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+	
+		ArrayList<String> arr1 = new ArrayList<>();
+		ArrayList<String> result = new ArrayList<>();
+		for(int i = 0; i < N; i++) {
+			arr1.add(br.readLine());
+		}
+		
+		for(int i = 0; i< M; i++) {
+			String str = br.readLine();
+			if(arr1.contains(str))
+				result.add(str);
+			
+		}
+		
+		Collections.sort(result);
+		
+		System.out.println(result.size());
+		for(String str : result) {
+			System.out.println(str);
+		}
+
+		
+	}
+
+}
