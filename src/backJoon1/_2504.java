ad6947680f40da35222254427b1d98a84786661b
userNameIldo <ildo5896@naver.com>
2022-03-16 17:00:59 +0900

    03.16

--- src/backJoon1/_2504.java
+++ src/backJoon1/_2504.java
@@ -16,33 +16,61 @@ public class _2504 {
 		
 		Stack<Character> stack = new Stack<>();
 		
-		int result = 1;
+		int result = 0;
+		int count = 1;
 		
 		for(int i = 0; i < c.length; i++) {
 			
 			if(c[i] == '(') {
 				stack.push(c[i]);
-				result *= 2;
+				count *= 2;
 			}else if(c[i] == '['){
 				stack.push(c[i]);
-				result *= 3;
+				count *= 3;
 			}else {
 				
 				if(c[i] == ')') {
 					
 					if(stack.empty() || stack.peek() != '(') {
 						result = 0;
+						break;
 					}else {
-						
+						if(c[i-1] == '(') {
+						result += count;
+						count = count/2;
+						stack.pop();
+						}else {
+							count = count/2;
+							stack.pop();	
+						}						
 					}
 					
+				}else if(c[i] == ']'){
+					
+					if(stack.empty() || stack.peek() != '[') {
+						result = 0;
+						break;
+					}else {
+						if(c[i-1] == '[') {
+							result += count;
+							count = count/3;
+							stack.pop();
+							}else {
+								count = count/3;
+								stack.pop();	
+							}					
+					}
+					
+				}else {
+					result = 0;
+					break;
 				}
-				
-				
 			}
 			
+		}if(!stack.empty()) {
+			result = 0;
 		}
-
+		System.out.println(result);
 	}
 
 }
